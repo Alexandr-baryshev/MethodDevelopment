a52ae1d265628c4977fc3f142d625d876fe4397a
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-04-28 01:02:17 +1000

    Save

--- src/next/Main.java
+++ src/next/Main.java
@@ -7,55 +7,42 @@ public class Main {
 
 
 
-        String v1 = "\t\t\t<textField>\n" +
-                "\t\t\t\t<reportElement style=\"Ячейка_C_root\" x=\"111\" y=\"$y\" width=\"$w\" height=\"$h\" uuid=\"$uuid\"/>\n" +
-                "\t\t\t\t<textFieldExpression><![CDATA[$V{R1 C1}]]></textFieldExpression>\n" +
-                "\t\t\t</textField>";
+        String v1 =  "<![CDATA[$V{R1 C1}]]>  x=\"55\" y=\"55\" width=\"20\" height=\"20\" uuid=\"59436ea4-a244-4f77-93a7-a4f8d020c83a\"/> \n";
 
 
-        String v2 = "Ячейка_C_root\" x=\"111\" y=\"555\" width";
 
-        System.out.println(poisk(v2));
+
+
+        System.out.println(sort_C(v1,1,3,1,1,3,1,0,0,30,30));
 
 
 
 
     }
-
-    public static String poisk (String jasperField) {
-        String result = "";
-        int x = 222;
-
-        UUID uuid = UUID.randomUUID();
-        String randomUUID = uuid.toString();
-
-//        result = jasperField.replace("$uuid", randomUUID);
-
-        result = jasperField.replaceAll("x=(\\s*)", "x=\"" + x +"");
-
-
-        return result;
-    }
-
-
-
-
 
 
     public static String sort_C(String jasperVar, int rStart, int rSize, int rStep,
                                                   int cStart, int cSize, int cStep,
-                                                        int x, int y, int w, int h ) {
+                                                  int xp, int yp, int wp, int hp ) {
 
         String row = "", column = "", allResult = "";
+        int x = xp;
 
         for (int c = cStart; c <= cSize; c = c + cStep) {
-
-            column = jasperVar.replace("C1", "C" + c).replace("$x", ""+x);
+            int y = yp;
+            column = jasperVar.replace("C1", "C" + c).replaceAll("x=(\\S+)", "x=\"" + x +"\"")
+                              .replaceAll("width=(\\S+)", "width=\"" + wp +"\"");
 
             for (int r = rStart; r <= rSize; r = r + rStep) {
-                row = column.replace("R1", "R" + r);
+                UUID uuid = UUID.randomUUID();
+                String randomUUID = uuid.toString();
+                row = column.replace("R1", "R" + r).replaceAll("y=(\\S+)", "y=\"" + y +"\"")
+                            .replaceAll("height=(\\S+)", "height=\"" + hp +"\"").replaceAll("uuid=(\\S+)", "uuid=\"" + randomUUID +"\"");
+
                 allResult = allResult + row + "\n";
+                y = y + (hp*rStep);
             }
+            x = x + wp;
         }
         return (allResult);
     }
