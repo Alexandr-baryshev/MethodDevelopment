6c250a235eb8b0ddcf183000f915ac4821103273
Alexandr <alexandr.baryshev@hotmail.com>
2020-04-28 12:27:36 +1000

    Save

--- src/next/GeneratorData.java
+++ src/next/GeneratorData.java
@@ -1,4 +1,34 @@
 package next;
 
 public class GeneratorData {
+
+   private String txtInput;
+
+   private int _Start_R;
+
+   private int C_Start;
+
+   public String getTxtInput() {
+      return txtInput;
+   }
+
+   public void setTxtInput(String txtInput) {
+      this.txtInput = txtInput;
+   }
+
+   public int get_Start_R() {
+      return _Start_R;
+   }
+
+   public void set_Start_R(int _Start_R) {
+      this._Start_R = _Start_R;
+   }
+
+   public int getC_Start() {
+      return C_Start;
+   }
+
+   public void setC_Start(int c_Start) {
+      C_Start = c_Start;
+   }
 }
